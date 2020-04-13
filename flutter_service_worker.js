44750b76e3105e670c45cf6b295ff57ad2395fe2
Wilson Wan <wilsonwansun@gmail.com>
2020-04-13 23:00:54 +0800

    Web Build From Mac Mini

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,14 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "80cadfc6a8f98c7637480522fc047d7e",
 "/": "80cadfc6a8f98c7637480522fc047d7e",
-"main.dart.js": "6de7305f0a1204c0376a270220aacfaa",
+"main.dart.js": "7af52d6cc69ac01de84a036d770cf052",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "d8bf28937e93cf4076dd19673372971e",
-"assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/LICENSE": "82245f830c8af42001c55b82ca626beb",
+"assets/images/logo.png": "3f7f1e170e5469c919764f3c0c052819",
+"assets/AssetManifest.json": "4c90a67f8169bac3a7fa280dcb3a60c5",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
